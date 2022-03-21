64aa0b08e2b9f017f65254277ef06220ba86b23a
pberlanda <paolo.berlanda@marconirovereto.it>
2022-03-21 19:35:03 +0100

    sistemazione commenti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
+  // stackblitz vuole il codice su una riga, attenzione agli spazi tra i tag
   template:'<button (click)="visible =!visible">Toggle</button> <h1 *ngIf="visible">ciao</h1>',
   //styleUrls: [ './app.component.css' ],
 })
