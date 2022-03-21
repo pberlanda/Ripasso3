f52b4f15d921d3c1a009e91b2cb5e57f725a5be2
pberlanda <paolo.berlanda@marconirovereto.it>
2022-03-21 19:14:14 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: <button (click)="visible"=!"visible">Toggle</button>
-  <h1 *NgIf="visible">hello world"</h1>
+  template: "<button (click)="visible"=!"visible">Toggle</button>
+  <h1 *NgIf="visible">hello world"</h1>"
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
