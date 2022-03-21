53cb2857b7a50f192284c0551ab91c37bbfc7bbc
pberlanda <paolo.berlanda@marconirovereto.it>
2022-03-21 19:38:00 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,10 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   // stackblitz vuole il codice su una riga, attenzione agli spazi tra i tag
-  template:'<button (click)="visible =!visible">Toggle</button> <h1 *ngIf="visible">ciao</h1>',
+  template:'<button (click)="visible =!visible">struca el boton</button> <h1 *ngIf="visible">ciao</h1>',
   //styleUrls: [ './app.component.css' ],
 })
-export class AppComponent  {
+export class AppComponent {
   //name = 'Angular ' + VERSION.major;
-  visible=false;
+  visible = true;
 }
