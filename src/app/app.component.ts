2c8b2166127fc5c7ad9fbe8d330cbd2364877d72
pberlanda <paolo.berlanda@marconirovereto.it>
2022-03-21 19:12:49 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { NgIf } from '@angular/common';
 import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
+  template: <button (click)="visible"=!"visible">Toggle</button>
+  <h1 *NgIf="visible">hello world"</h1>
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  visible=true;
 }
